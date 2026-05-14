900f39fba9cbdcb128b27c42c8d423c24cd886ff
JuanUribeDv <juanuribe079@gmail.com>
2026-05-13 20:23:03 -0500

    Se cambió el diseño del dashboard, del form del inventario etc

---
+++ app/src/main/java/com/example/innovacode_proyecto1/Inventarioform/Content/Inventarioform.kt
@@ -0,0 +1,28 @@
+package com.example.innovacode_proyecto1.Inventarioform.Content
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.innovacode_proyecto1.R
+
+
+class Inventarioform : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inventarioform)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+    val btn_cancelar = findViewById<Button>(R.id.btnCancel)
+    val btn_Guardar=findViewById<Button>(R.id.btnSave)
+
+
+}
